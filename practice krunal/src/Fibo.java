91b28b42c574e03ba9a88c143adc35f1c508a7b2
Dhruvin Gadhiya <200533025@student.georgianc.on.ca>
2023-02-15 13:47:35 -0500

    Upgraded with  O(n) time complexity

--- practice krunal/src/Fibo.java
+++ practice krunal/src/Fibo.java
@@ -1,18 +1,26 @@
-import java.util.Scanner;
-
 public class Fibo {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = 0;
-        int b =1;
-        int constantStart=2;
-        while(constantStart<=n){
-          int temparory = b;
-            b+=a;
-            constantStart++;
-            a=temparory;
-        }
-        System.out.println(b);
+//        Scanner sc= new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int a = 0;
+//        int b =1;
+//        int constantStart=2;
+//        while(constantStart<=n){
+//          int temparory = b;
+//            b+=a;
+//            constantStart++;
+//            a=temparory;
+//        }
+//        System.out.println(b);
+//    }
+
+        // with decreased time complexity
+
+        //  f(n) = f(n-1) + f(n-2)
+        //  @^n = @^(n-1) + @^(n-2)
+
+        int n = 50;
+        int fibo = (int) (Math.pow((1 + Math.sqrt(5)) / 2,n) / Math.sqrt(5));
+        System.out.println(fibo);
     }
 }
